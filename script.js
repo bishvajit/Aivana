a0ac5d6d7d5aa7cff5d6a8111c94f5b65da4cd37
bishvajit <112610641+bishvajit@users.noreply.github.com>
2025-07-15 02:17:59 +0530

    make new changes

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("E-Commerce Website Loaded");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('open');
+  });
+});
